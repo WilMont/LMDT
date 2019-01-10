99b655a8e35c00aa9411a2736bbf71df3fce0279
Alexandre GAULTIER <alexandre.gaultier@epsi.fr>
2019-01-10 14:22:19 +0100

    Paste de "RechercheParNormes.xaml.cs" effectué

--- Prototype/Prototype/RechecheParNormesPage.xaml.cs
+++ Prototype/Prototype/RechecheParNormesPage.xaml.cs
@@ -9,12 +9,29 @@ using Xamarin.Forms.Xaml;
 
 namespace Prototype
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class RechecheParNormesPage : ContentPage
-	{
-		public RechecheParNormesPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RechercheParNormePage : ContentPage
+    {
+        public RechercheParNormePage()
+        {
+            InitializeComponent();
+
+            var listNormes = new ListView();
+            listNormes.ItemsSource = new string[]
+            {
+                "METRIQUE / M",
+                "METRIQUE FIN / MF",
+                "AMERICAIN / UNC",
+                "AMERICAIN FIN / UNF",
+                "AMERICAIN EXTRA FIN / UNEF",
+                "ANGLAIS / BSW",
+                "ANGLAIS FIN / BSF",
+                "GAZ CYLINDRIQUE / BSP",
+                "GAZ CONIQUE AMERICAIN / NPT",
+                "GAZ CONIQUE ANGLAIS / BSPT",
+                "PAS ELECTRIQUE / PT",
+                "TRAPEZE / TR",
+            };
+        }
+    }
 }
