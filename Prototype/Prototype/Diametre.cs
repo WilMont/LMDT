04d2fc3eea6156259cff733dd2f80a12085ee564
Wilfried MONTAGNE <wilfired.montagne@epsi.fr>
2019-01-11 17:00:57 +0100

    - Ajout des pages de Conseils et autres

---
+++ Prototype/Prototype/Diametre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Diametre
+    {
+        public string Valeur { get; set; }
+
+        public override string ToString()
+        {
+            return Valeur;
+        }
+    }
+}
