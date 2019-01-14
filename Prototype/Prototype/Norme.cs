a11df642098b5bc7e9b6b6bbd038a198af0ac9fc
Alexandre GAULTIER <alexandre.gaultier@epsi.fr>
2019-01-14 12:03:03 +0100

    Systeme de liste Recherche par diametre fait !

---
+++ Prototype/Prototype/Norme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Norme
+    {
+        public string Nom { get; set; }
+
+        public override string ToString()
+        {
+            return Nom;
+        }
+    }
+}
