9b6813b79748fb34c510375790813b228853fa77
Alexandre GAULTIER <alexandre.gaultier@epsi.fr>
2019-01-14 12:03:03 +0100

    Systeme de liste Recherche par diametre fait !

--- Prototype/Prototype/RechercheParDiametrePage.xaml.cs
+++ Prototype/Prototype/RechercheParDiametrePage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +15,76 @@ namespace Prototype
 		{
 			InitializeComponent ();
 		}
-	}
+
+
+        public bool EtatTabDiametre = false;
+        public bool EtatTabPas = false;
+        public bool EtatTabPerçage = false;
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///
+        public void ButtonChoixDiametre(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Diametre.IsVisible = true;
+                BouttonChoixDiametre.IsVisible = false;
+                BouttonChoixPas.IsVisible = false;
+                BouttonChoixPerçage.IsVisible = false;
+                  });
+        }
+
+        public void Retour_Diametre(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Diametre.IsVisible = false;
+                BouttonChoixDiametre.IsVisible = true;
+                BouttonChoixPas.IsVisible = true;
+                BouttonChoixPerçage.IsVisible = true;
+            });
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public void ButtonChoixPas(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Pas.IsVisible = true;
+                BouttonChoixDiametre.IsVisible = false;
+                BouttonChoixPas.IsVisible = false;
+                BouttonChoixPerçage.IsVisible = false;
+            });
+        }
+
+        public void Retour_Pas(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Pas.IsVisible = false;
+                BouttonChoixDiametre.IsVisible = true;
+                BouttonChoixPas.IsVisible = true;
+                BouttonChoixPerçage.IsVisible = true;
+            });
+        }
+        
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public void ButtonChoixPerçage(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Perçage.IsVisible = true;
+                BouttonChoixDiametre.IsVisible = false;
+                BouttonChoixPas.IsVisible = false;
+                BouttonChoixPerçage.IsVisible = false;
+            });
+        }
+
+        public void Retour_Perçage(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Perçage.IsVisible = false;
+                BouttonChoixDiametre.IsVisible = true;
+                BouttonChoixPas.IsVisible = true;
+                BouttonChoixPerçage.IsVisible = true;
+            });
+        }
+
+    }
 }
