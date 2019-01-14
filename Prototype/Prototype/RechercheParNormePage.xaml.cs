52ef44195f2d247869543f46346c27e786f05d17
Wilfried MONTAGNE <wilfired.montagne@epsi.fr>
2019-01-14 20:04:04 +0100

    - Mise en place de boutons pour chaques normes.

--- Prototype/Prototype/RechercheParNormePage.xaml.cs
+++ Prototype/Prototype/RechercheParNormePage.xaml.cs
@@ -17,6 +17,61 @@ namespace Prototype
             InitializeComponent();
         }
 
+        async void BtnNormeMetrique_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeMetriqueFin_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeAmericain_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeAmericainFin_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeAmericainExtraFin_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeAnglais_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeAnglaisFin_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeGazCylindrique_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeGazConiqueAmericain_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormeGazConiqueAnglais_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
+        async void BtnNormePasElectrique_Clicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+
         async void BtnNormeTrapeze_Clicked(object sender, EventArgs args)
         {
             await Navigation.PushAsync(new NormeTrapezePage());
