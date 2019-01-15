68893d21dd188dbaf6d4c61b891ae1f47fb27021
Alexandre GAULTIER <alexandre.gaultier@epsi.fr>
2019-01-15 16:49:01 +0100

    Tout les liens vers les NormesPages sont fonctionnels. Il y a les boutons de chaque normes à changer et 2 3 trucs à changer

---
+++ Prototype/Prototype/Niv2/RechercheParNormePage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Prototype.Niv3.Normes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RechercheParNormePage : ContentPage
+    {
+        public RechercheParNormePage()
+        {
+            InitializeComponent();
+        }
+
+        async void Metrique_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeMetriquePage());
+        }
+
+        async void MetriqueFin_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeMetriqueFinPage());
+        }
+
+        async void Americain_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeAmericainPage());
+        }
+
+        async void AmericainFin_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeAmericainFinPage());
+        }
+
+        async void AmericainExtraFin_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeAmericainExtraFinPage());
+        }
+
+        async void Anglais_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeAnglaisPage());
+        }
+
+        async void AnglaisFin_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeAnglaisFinPage());
+        }
+
+        async void GazCylindrique_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeGazCylindriquePage());
+        }
+
+        async void GazCylindriqueAmericain_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeGazConiqueAmericainPage());
+        }
+
+        async void PasElectrique_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormePasElectriquePage());
+        }
+
+        async void Trapeze_Button(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new NormeTrapezePage());
+        }
+    }
+}
---
+++ Prototype/Prototype/Niv3/Normes/NormeGazConiqueAmericainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype.Niv3.Normes
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NormeGazConiqueAmericainPage : ContentPage
+	{
+		public NormeGazConiqueAmericainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
