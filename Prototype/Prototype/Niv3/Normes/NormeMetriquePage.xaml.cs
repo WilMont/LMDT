c3bda6598afdaad54e0c8dc2ebc26bcdb4ec06d6
Wilfried MONTAGNE <wilfired.montagne@epsi.fr>
2019-01-16 11:48:54 +0100

    - Ajout de liste aux boutons Diamètre des la norme Metrique et Metrique Fin.

--- Prototype/Prototype/Niv3/Normes/NormeMetriquePage.xaml.cs
+++ Prototype/Prototype/Niv3/Normes/NormeMetriquePage.xaml.cs
@@ -16,5 +16,70 @@ namespace Prototype.Normes
 		{
 			InitializeComponent ();
 		}
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///
+        public void ButtonChoixDiametre(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Diametre.IsVisible = true;
+                BouttonChoixDiametre.IsVisible = false;
+                BouttonChoixPas.IsVisible = false;
+                BouttonChoixForet.IsVisible = false;
+            });
+        }
+
+        public void Retour_Diametre(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Diametre.IsVisible = false;
+                BouttonChoixDiametre.IsVisible = true;
+                BouttonChoixPas.IsVisible = true;
+                BouttonChoixForet.IsVisible = true;
+            });
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public void ButtonChoixPas(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Pas.IsVisible = true;
+                BouttonChoixDiametre.IsVisible = false;
+                BouttonChoixPas.IsVisible = false;
+                BouttonChoixForet.IsVisible = false;
+            });
+        }
+
+        public void Retour_Pas(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Pas.IsVisible = false;
+                BouttonChoixDiametre.IsVisible = true;
+                BouttonChoixPas.IsVisible = true;
+                BouttonChoixForet.IsVisible = true;
+            });
+        }
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public void ButtonChoixForet(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Foret.IsVisible = true;
+                BouttonChoixDiametre.IsVisible = false;
+                BouttonChoixPas.IsVisible = false;
+                BouttonChoixForet.IsVisible = false;
+            });
+        }
+
+        public void Retour_Foret(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                Tab_Foret.IsVisible = false;
+                BouttonChoixDiametre.IsVisible = true;
+                BouttonChoixPas.IsVisible = true;
+                BouttonChoixForet.IsVisible = true;
+            });
+        }
     }
 }
