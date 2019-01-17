86fa58f1a8fb81349aac25dad78b09b27a7a86d6
Alexandre GAULTIER <alexandre.gaultier@epsi.fr>
2019-01-17 15:47:24 +0100

    Conseils PARFAIT fonctonnels plus beaux et Convertisseurs juste beaux, fonctonnalité à faire des que valuers obtennuent

--- Prototype/Prototype/Niv3/Conseils/ConseilsIncidentsEtResolutionsPage.xaml.cs
+++ Prototype/Prototype/Niv3/Conseils/ConseilsIncidentsEtResolutionsPage.xaml.cs
@@ -16,5 +16,115 @@ namespace Prototype
 		{
 			InitializeComponent ();
 		}
-	}
+
+        bool Incident1 = false;
+        public void TaraudageTropGrand(object sender, EventArgs args)
+        {
+            if (Incident1 == false)
+            {
+
+                Device.BeginInvokeOnMainThread(() => {
+                    TropGrand.IsVisible = true;
+
+                    Incident1 = true;
+                });
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() => {
+                    TropGrand.IsVisible = false;
+
+                    Incident1 = false;
+                });
+            }
+        }
+
+        bool Incident2 = false;
+        public void TaraudageTropSerré(object sender, EventArgs args)
+        {
+            if (Incident2 == false)
+            {
+
+                Device.BeginInvokeOnMainThread(() => {
+                    TropSerré.IsVisible = true;
+
+                    Incident2 = true;
+                });
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() => {
+                    TropSerré.IsVisible = false;
+
+                    Incident2 = false;
+                });
+            }
+        }
+
+        bool Incident3 = false;
+        public void TaraudageCassé(object sender, EventArgs args)
+        {
+            if (Incident3 == false)
+            {
+
+                Device.BeginInvokeOnMainThread(() => {
+                    Cassé.IsVisible = true;
+
+                    Incident3 = true;
+                });
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() => {
+                    Cassé.IsVisible = false;
+
+                    Incident3 = false;
+                });
+            }
+        }
+
+        bool Incident4 = false;
+        public void TaraudAbimé(object sender, EventArgs args)
+        {
+            if (Incident4 == false)
+            {
+
+                Device.BeginInvokeOnMainThread(() => {
+                    MauvaisEtat.IsVisible = true;
+
+                    Incident4 = true;
+                });
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() => {
+                    MauvaisEtat.IsVisible = false;
+
+                    Incident4 = false;
+                });
+            }
+        }
+
+        bool Incident5 = false;
+        public void DuréeDeVieTropCourte(object sender, EventArgs args)
+        {
+            if (Incident5 == false)
+            {
+
+                Device.BeginInvokeOnMainThread(() => {
+                    CourteVie.IsVisible = true;
+
+                    Incident5 = true;
+                });
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() => {
+                    CourteVie.IsVisible = false;
+
+                    Incident5 = false;
+                });
+            }
+        }
+    }
 }
