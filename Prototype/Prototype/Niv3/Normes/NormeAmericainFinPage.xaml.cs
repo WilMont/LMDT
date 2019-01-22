b4f4b0cd882010864ad1817303e3b72a56292c7f
Alexandre GAULTIER <alexandre.gaultier@epsi.fr>
2019-01-22 13:45:34 +0100

    Norme Americain Fin ajouté !

--- Prototype/Prototype/Niv3/Normes/NormeAmericainFinPage.xaml.cs
+++ Prototype/Prototype/Niv3/Normes/NormeAmericainFinPage.xaml.cs
@@ -16,5 +16,46 @@ namespace Prototype
 		{
 			InitializeComponent ();
 		}
-	}
+        public void ButtonChoixDiametre(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Tab_Diametre.IsVisible = true;
+                TabBtnLabelDiametre.IsVisible = false;
+                TabBtnLabelPas.IsVisible = false;
+                TabBtnLabelPerçage.IsVisible = false;
+            });
+        }
+
+        public void Retour_Diametre(object sender, EventArgs args)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Tab_Diametre.IsVisible = false;
+                TabBtnLabelDiametre.IsVisible = true;
+                TabBtnLabelPas.IsVisible = true;
+                TabBtnLabelPerçage.IsVisible = true;
+            });
+        }
+
+        void Button_Click(object sender, EventArgs args)
+        {
+            Button btn = (Button)sender;
+
+            var textDiametre = btn.Text.Substring(0, 7);
+            var textPas = btn.Text.Substring(8, 2);
+            var textForet = btn.Text.Substring(11, 5);
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Tab_Diametre.IsVisible = false;
+                TabBtnLabelDiametre.IsVisible = true;
+                TabBtnLabelPas.IsVisible = true;
+                TabBtnLabelPerçage.IsVisible = true;
+                BouttonChoixDiametre.Text = textDiametre;
+                BouttonChoixPas.Text = textPas;
+                BouttonChoixForet.Text = textForet;
+            });
+        }
+    }
 }
