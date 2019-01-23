04940c6db096cac1fee07fa172ccf9efd8583b3d
Alexandre GAULTIER <alexandre.gaultier@epsi.fr>
2019-01-23 15:46:57 +0100

    Ajustements graphiques et convertisseur MM Pouces Fait !

--- Prototype/Prototype/Niv3/Convertisseur/ConvertisseurMMenPoucePage.xaml.cs
+++ Prototype/Prototype/Niv3/Convertisseur/ConvertisseurMMenPoucePage.xaml.cs
@@ -9,19 +9,25 @@ using Xamarin.Forms.Xaml;
 
 namespace Prototype
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ConvertisseurMMenPouce : ContentPage
-	{
-		public ConvertisseurMMenPouce ()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConvertisseurMMenPouce : ContentPage
+    {
+        public ConvertisseurMMenPouce()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         ///
         public void ButtonChoixMM(object sender, EventArgs args)
         {
-            Device.BeginInvokeOnMainThread(() => {
+            Device.BeginInvokeOnMainThread(() =>
+            {
                 Tab_MM.IsVisible = true;
                 BouttonChoixMM.IsVisible = false;
                 BouttonChoixPouces.IsVisible = false;
@@ -34,7 +40,8 @@ namespace Prototype
 
         public void Retour_millimètre(object sender, EventArgs args)
         {
-            Device.BeginInvokeOnMainThread(() => {
+            Device.BeginInvokeOnMainThread(() =>
+            {
                 Tab_MM.IsVisible = false;
                 BouttonChoixMM.IsVisible = true;
                 BouttonChoixPouces.IsVisible = true;
@@ -48,7 +55,8 @@ namespace Prototype
 
         public void ButtonChoixPouces(object sender, EventArgs args)
         {
-            Device.BeginInvokeOnMainThread(() => {
+            Device.BeginInvokeOnMainThread(() =>
+            {
                 Tab_Pouces.IsVisible = true;
                 BouttonChoixMM.IsVisible = false;
                 BouttonChoixPouces.IsVisible = false;
@@ -61,7 +69,8 @@ namespace Prototype
 
         public void Retour_Pouce(object sender, EventArgs args)
         {
-            Device.BeginInvokeOnMainThread(() => {
+            Device.BeginInvokeOnMainThread(() =>
+            {
                 Tab_Pouces.IsVisible = false;
                 BouttonChoixMM.IsVisible = true;
                 BouttonChoixPouces.IsVisible = true;
@@ -69,6 +78,50 @@ namespace Prototype
                 LabelBtn1.IsVisible = true;
                 LabelBtn2.IsVisible = true;
                 FrameImage.IsVisible = true;
+            });
+        }
+
+        void Button_Click_MM(object sender, EventArgs args)
+        {
+            Button btn = (Button)sender;
+
+            var text_MM = btn.Text.Substring(0, 6);
+            var text_Pouces = btn.Text.Substring(7, 9);
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Tab_MM.IsVisible = false;
+                LabelBtn1.IsVisible = true;
+                BouttonChoixMM.IsVisible = true;
+                LabelBtn2.IsVisible = true;
+                BouttonChoixPouces.IsVisible = true;
+                ImageConversion.IsVisible = true;
+                FrameImage.IsVisible = true;
+
+                BouttonChoixMM.Text = text_MM;
+                BouttonChoixPouces.Text = text_Pouces;
+            });
+        }
+
+        void Button_Click_Pouces(object sender, EventArgs args)
+        {
+            Button btn = (Button)sender;
+
+            var text_MM = btn.Text.Substring(10, 6);
+            var text_Pouces = btn.Text.Substring(0, 9);
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Tab_Pouces.IsVisible = false;
+                LabelBtn1.IsVisible = true;
+                BouttonChoixMM.IsVisible = true;
+                LabelBtn2.IsVisible = true;
+                BouttonChoixPouces.IsVisible = true;
+                ImageConversion.IsVisible = true;
+                FrameImage.IsVisible = true;
+
+                BouttonChoixMM.Text = text_MM;
+                BouttonChoixPouces.Text = text_Pouces;
             });
         }
     }
